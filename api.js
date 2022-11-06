baacacf6bffa51a2ef1c77aa7f2411ca5df0d45c
Munniza <you@example.com>
2022-11-06 19:12:08 +0100

    done

---
+++ api.js
@@ -0,0 +1,23 @@
+
+async function jokes () {
+    const res = await fetch ("https://official-joke-api.appspot.com/random_joke");
+    const data = await res.json();
+    // console.log("res:", res);
+    // console.log("data:", data);
+    const setup = data.setup;
+    console.log(data.setup)
+    const punchline = data.punchline;
+    document.getElementById('myjokes').innerHTML = setup + " ";
+
+    document.getElementById("button").addEventListener("click", myFunction);
+
+function myFunction() {
+    document.getElementById("mypunchline").innerHTML = punchline;
+
+}
+    }
+    
+    jokes();
+
+
+
